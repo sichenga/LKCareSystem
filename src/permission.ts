65e1e99e31177917d803a7d255e4b848379d766d
xujingxin <3181959755@qq.com>
2024-05-07 16:54:41 +0800

    修改路由

--- src/permission.ts
+++ src/permission.ts
@@ -1,17 +1,17 @@
 import router from '@/router'
 import {useUserStore  } from '@/stores'
 // 路由守卫
-router.beforeEach((to) => {
-    const userStore = useUserStore()
-    if(userStore.token){
+// router.beforeEach((to) => {
+//     const userStore = useUserStore()
+//     if(userStore.token){
       
-      return true
-    }else{
-      console.log(1);
-        if(to.path == '/login'){
-            return true
-        }else{
-            return '/login'
-        }
-    }
-})
+//       return true
+//     }else{
+//       console.log(1);
+//         if(to.path == '/login'){
+//             return true
+//         }else{
+//             return '/login'
+//         }
+//     }
+// })
