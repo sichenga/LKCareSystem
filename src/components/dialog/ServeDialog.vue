99d40be8a7f43b075709035b8cae3fe13b0301a3
xujingxin <3181959755@qq.com>
2024-05-17 19:47:44 +0800

    dialog封装

--- src/components/dialog/ServeDialog.vue
+++ src/components/dialog/ServeDialog.vue
@@ -1,33 +1,62 @@
 <template>
-    <el-dialog v-model="dialogVisible" title="新增/编辑护理服务" width="500" @close="close">
-        <el-form ref="ruleFormRef" style="max-width: 600px" :model="ruleForm" :rules="rules" label-width="auto"
-            class="demo-ruleForm" :size="formSize" status-icon>
-            <el-form-item label="服务名称" prop="name">
-                <el-input v-model="ruleForm.name" />
-            </el-form-item>
-            <el-form-item label="关联表单">
-                <el-select v-model="ruleForm.region" placeholder="请选择">
-                    <el-option label="Zone one" value="shanghai" />
-                    <el-option label="Zone two" value="beijing" />
-                </el-select>
-            </el-form-item>
-            <el-form-item label="服务描述" prop="desc">
-                <el-input v-model="ruleForm.desc" type="textarea" />
-            </el-form-item>
-        </el-form>
-        <template #footer>
-            <div class="dialog-footer">
-                <el-button @click="close">取消</el-button>
-                <el-button type="primary" @click="close(true)">确定</el-button>
-            </div>
-        </template>
-    </el-dialog>
+  <!-- <el-dialog v-model="dialogVisible" title="新增/编辑护理服务" width="500" @close="close">
+    <el-form
+      ref="ruleFormRef"
+      style="max-width: 600px"
+      :model="ruleForm"
+      :rules="rules"
+      label-width="auto"
+      class="demo-ruleForm"
+      :size="formSize"
+      status-icon
+    >
+      <el-form-item label="服务名称" prop="name">
+        <el-input v-model="ruleForm.name" />
+      </el-form-item>
+      <el-form-item label="关联表单">
+        <el-select v-model="ruleForm.region" placeholder="请选择">
+          <el-option label="Zone one" value="shanghai" />
+          <el-option label="Zone two" value="beijing" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="服务描述" prop="desc">
+        <el-input v-model="ruleForm.desc" type="textarea" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="close">取消</el-button>
+        <el-button type="primary" @click="close(true)">确定</el-button>
+      </div>
+    </template>
+  </el-dialog> -->
+  <MayDialog :title="'新增护理服务'" @close="close">
+    <template #dialog>
+      <el-form
+        ref="ruleFormRef"
+        style="max-width: 600px"
+        :model="ruleForm"
+        :rules="rules"
+        label-width="auto"
+        class="demo-ruleForm"
+        :size="formSize"
+        status-icon
+      >
+        <el-form-item label="服务名称" prop="name">
+          <el-input v-model="ruleForm.name" />
+        </el-form-item>
+        <el-form-item label="服务描述" prop="desc">
+          <el-input v-model="ruleForm.desc" type="textarea" />
+        </el-form-item>
+      </el-form>
+    </template>
+  </MayDialog>
 </template>
 
 <script lang="ts" setup>
-import { reactive, ref } from 'vue'
+import { reactive, ref, defineEmits } from 'vue'
 import type { ComponentSize, FormInstance, FormRules } from 'element-plus'
-
+import MayDialog from '@/components/dialog/maydialog/MayDialog.vue'
 interface RuleForm {
   name: string
   region: string
@@ -39,40 +68,26 @@ const ruleFormRef = ref<FormInstance>()
 const ruleForm = reactive<RuleForm>({
   name: 'Hello',
   region: '',
-  desc: '',
+  desc: ''
 })
 
-
-
 const rules = reactive<FormRules<RuleForm>>({
-  name: [
-    { required: true, message: 'Please input Activity name', trigger: 'blur' },
-  ],
+  name: [{ required: true, message: 'Please input Activity name', trigger: 'blur' }],
   region: [
     {
       required: true,
       message: 'Please select Activity zone',
-      trigger: 'change',
-    },
+      trigger: 'change'
+    }
   ],
-  desc: [
-    { required: true, message: 'Please input activity form', trigger: 'blur' },
-  ],
+  desc: [{ required: true, message: 'Please input activity form', trigger: 'blur' }]
 })
 
-
-
-
-
-//弹框
-const dialogVisible = ref(true)
+//关闭弹框
 const emit = defineEmits(['close'])
 const close = (close: boolean = false) => {
-    emit('close', close)
+  emit('close', close)
 }
 </script>
 
-<style lang="less" scoped>
-
-
-</style>
+<style lang="less" scoped></style>
--- src/views/config/nursingServiceView.vue
+++ src/views/config/nursingServiceView.vue
@@ -1,57 +1,58 @@
 <template>
-    <!-- 护理服务 -->
+  <!-- 护理服务 -->
   <el-card style="max-width: 100%">
-    <el-button type="primary"  @click="isdialog = true">新增服务</el-button>
-        <ServeDialog @close="close" v-if="isdialog"></ServeDialog>
-        <MayTable :tableData="data.tableData" :tableItem="data.tableItem">
-            <template #operate>
-                <el-button type="primary" text >编辑</el-button>
-                <el-button type="primary" text @click="del">删除</el-button>
-            </template>
-        </MayTable>
+    <el-button type="primary" @click="add()">新增服务</el-button>
+    <ServeDialog @close="close" v-if="isdialog"></ServeDialog>
+    <MayTable :tableData="data.tableData" :tableItem="data.tableItem">
+      <template #operate>
+        <el-button type="primary" text>编辑</el-button>
+        <el-button type="primary" text @click="del">删除</el-button>
+      </template>
+    </MayTable>
   </el-card>
 </template>
 <script lang="ts" setup>
-import { ref,reactive,defineAsyncComponent,onMounted } from 'vue'
+import { ref, reactive, defineAsyncComponent, onMounted } from 'vue'
 import ServeView from '@/database/ServeView.json'
-import ServeDialog from '@/components/dialog/ServeDialog.vue';
+import ServeDialog from '@/components/dialog/ServeDialog.vue'
 const MayTable = defineAsyncComponent(() => import('@/components/table/MayTable.vue'))
 
+const isdialog = ref(false)
 const data = reactive({
-    tableData: [] as any,
-    tableItem: [
-        {
-            prop: 'id',
-            label: '序号',
-            width:'60'
-        },
-        {
-            prop: 'name',
-            label: '服务名称',
-            width:'100'
-
-        },
-        {
-            prop: 'describe',
-            label: '服务描述'
-        },
-     
-
-    ]
+  tableData: [] as any,
+  tableItem: [
+    {
+      prop: 'id',
+      label: '序号',
+      width: '60'
+    },
+    {
+      prop: 'name',
+      label: '服务名称',
+      width: '100'
+    },
+    {
+      prop: 'describe',
+      label: '服务描述'
+    }
+  ]
 })
 const getlist = () => {
-    setTimeout(() => {
-        data.tableData = ServeView
-    }, 1000)
+  setTimeout(() => {
+    data.tableData = ServeView
+  }, 1000)
 }
-onMounted(() => {
-    getlist()
-})
+
 //弹出框
-const isdialog = ref(false)
 const close = () => {
   isdialog.value = false
 }
+
+// 新增
+const add = () => {
+  isdialog.value = true
+}
+
 //删除
 import { getMessageBox } from '@/utils/utils'
 import { ElMessage } from 'element-plus'
@@ -64,10 +65,14 @@ const del = async () => {
     ElMessage.info('取消删除')
   }
 }
+
+onMounted(() => {
+  getlist()
+})
 </script>
 
 <style lang="less" scoped>
 .el-button {
-    margin-bottom: 20px;
+  margin-bottom: 20px;
 }
 </style>
