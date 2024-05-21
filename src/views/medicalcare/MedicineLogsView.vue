7db9a6251401a03e8e086ad41c6c2af1a78f50bc
xujingxin <3181959755@qq.com>
2024-05-21 20:45:03 +0800

    修改文件大小写

---
+++ src/views/medicalcare/MedicineLogsView.vue
@@ -0,0 +1,91 @@
+<template>
+  <!-- 用药登记 -->
+  <el-card>
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="老人：">
+        <el-input v-model="formInline.user" placeholder="请输入" clearable />
+      </el-form-item>
+      <el-form-item label="登记时间：">
+        <TimePicker></TimePicker>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary">查询</el-button>
+        <el-button>重置</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
+  <el-card style="margin-top: 15px">
+    <!-- 表格 -->
+    <MayTable :tableData="data.tableData" :tableItem="data.tableItem">
+      <template #operate>
+        <el-button type="primary" text @click="registerinfo">用药登记</el-button>
+      </template>
+    </MayTable>
+    <Pagination :total="50"></Pagination>
+  </el-card>
+</template>
+<script lang="ts" setup>
+import { ref, reactive, onMounted, defineAsyncComponent } from 'vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+import AffiliatedView from '@/database/AffiliatedView.json'
+const MayTable = defineAsyncComponent(() => import('@/components/table/MayTable.vue'))
+const Pagination = defineAsyncComponent(() => import('@/components/pagination/MayPagination.vue'))
+const TimePicker = defineAsyncComponent(() => import('@/components/timepicker/MayTimePicker.vue'))
+const formInline = reactive({
+  user: '',
+  region: '',
+  date: ''
+})
+const data = reactive({
+  tableData: [] as any,
+  tableItem: [
+    {
+      prop: 'id',
+      label: '序号'
+    },
+    {
+      prop: 'name',
+      label: '老人姓名'
+    },
+    {
+      prop: 'address',
+      label: '用药品种类'
+    },
+    {
+      prop: 'manager',
+      label: '登记人'
+    },
+    {
+      prop: 'phone',
+      label: '最新登记时间'
+    }
+  ]
+})
+const getlist = () => {
+  setTimeout(() => {
+    data.tableData = AffiliatedView
+  }, 1000)
+}
+// 用药登记
+const registerinfo = () => {
+  router.push({
+    path: '/medicalcare/medicinelogs/details/id'
+  })
+}
+
+onMounted(() => {
+  getlist()
+})
+</script>
+<style lang="less" scoped>
+.el-input {
+  height: 40px;
+}
+
+.el-button {
+  height: 40px;
+  line-height: 40px;
+}
+</style>
